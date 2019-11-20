41b21693a1bf0412174cd8adb96e443c127c1a58
Jeremiah Cox <17728431+out0xb2@users.noreply.github.com>
2019-11-20 15:52:11 -0800

    Converted Intel Silicon Tools helper to a build plug-in    The plug-in pre-build will build BaseTools / FitGen.exe    The plug-in post-build will execute FitGen.exe if needed PlatformBuild cleanup Works/tested

--- Platform/Intel/KabylakeOpenBoardPkg/KabylakeRvp3/PlatformBuild.py
+++ Platform/Intel/KabylakeOpenBoardPkg/KabylakeRvp3/PlatformBuild.py
@@ -7,13 +7,11 @@
 import os
 import logging
 
-from edk2toolext.environment import shell_environment
 from edk2toolext.environment.uefi_build import UefiBuilder
 from edk2toolext.invocables.edk2_platform_build import BuildSettingsManager
 from edk2toolext.invocables.edk2_setup import SetupSettingsManager, RequiredSubmodule
 from edk2toolext.invocables.edk2_update import UpdateSettingsManager
 from edk2toollib.utility_functions import RunPythonScript
-
 
     # ####################################################################################### #
     #                                Common Configuration                                     #
@@ -22,20 +20,13 @@ class CommonPlatform():
     ''' Common settings for this platform.  Define static data here and use
         for the different parts of stuart
     '''
-    WorkspaceRoot = os.path.realpath(os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "..\..\..\.."))
-    ProductName = "KblRvp3"
+    WorkspaceRoot = os.path.realpath(os.path.join( os.path.dirname(os.path.abspath(__file__)), 
+                    "..\..\..\.."))
+    ProductName = "KabylakeRvp3"
     BoardPackage = "KabylakeOpenBoardPkg"
-    Project = "KabylakeRvp3"
-    Platform = "OpenBoardPkg.dsc"
-    PackagesSupported = (BoardPackage,)
     ArchSupported = ("IA32", "X64")
     TargetsSupported = ("DEBUG", "RELEASE", "NOOPT")
-    Scopes = ('edk2-build','kbl','intel_fsp')
-    FspBinPath = "Silicon/Intel/FSPS/AmberLakeFspBinPkg/"
-    FspRebaseScript = os.path.join("Platform","Intel","MinPlatformPkg","Tools","Fsp","RebaseFspBinBaseAddress.py")
-    FlashMap = os.path.join("Platform","Intel",BoardPackage,Project,
-                            "Include","Fdf","FlashMapInclude.fdf")
+    Scopes = ('edk2-build','intel_silicon_tools','intel_fsp')
 
 
     # ####################################################################################### #
@@ -46,7 +37,7 @@ class SettingsManager(UpdateSettingsManager, SetupSettingsManager):
     def GetPackagesSupported(self):
         ''' return iterable of edk2 packages supported by this build.
         These should be edk2 workspace relative paths '''
-        return CommonPlatform.PackagesSupported
+        return (BoardPackage,)
 
     def GetArchitecturesSupported(self):
         ''' return iterable of edk2 architectures supported by this build '''
@@ -61,8 +52,7 @@ class SettingsManager(UpdateSettingsManager, SetupSettingsManager):
         If no RequiredSubmodules return an empty iterable
         '''
         rs = []
-        rs.append(RequiredSubmodule(
-            "EDK2", True))
+        rs.append(RequiredSubmodule("EDK2", True))
         return rs
 
     def SetArchitectures(self, list_of_requested_architectures):
@@ -86,7 +76,6 @@ class SettingsManager(UpdateSettingsManager, SetupSettingsManager):
         ''' return tuple containing scopes that should be active for this process '''
         return CommonPlatform.Scopes
 
-
     # ####################################################################################### #
     #                         Actual Configuration for Platform Build                         #
     # ####################################################################################### #
@@ -96,15 +85,21 @@ class PlatformBuilder( UefiBuilder, BuildSettingsManager):
 
     def SetPlatformEnv(self):
         logging.debug("PlatformBuilder SetPlatformEnv")
-        Project = "/".join((CommonPlatform.BoardPackage,CommonPlatform.Project))
-        Platform = "/".join((Project,CommonPlatform.Platform))
-        self.env.SetValue("PRODUCT_NAME",                 CommonPlatform.ProductName,             "Platform Hardcoded")
-        self.env.SetValue("TARGET_ARCH",                  " ".join((CommonPlatform.ArchSupported)), "Platform Hardcoded")
-        self.env.SetValue("BLD_*_PLATFORM_BOARD_PACKAGE", CommonPlatform.BoardPackage,            "Platform Hardcoded")
-        self.env.SetValue("BLD_*_PROJECT",                Project,                                "Platform Hardcoded")
-        self.env.SetValue("ACTIVE_PLATFORM",              Platform,                               "Platform Hardcoded")
-        self.env.SetValue("FSP_BINARY_PATH",              CommonPlatform.FspBinPath,              "Platform Hardcoded")
-        self.env.SetValue("TOOL_CHAIN_TAG",               "VS2017",                               "Default tool chain")
+
+        Project = CommonPlatform.BoardPackage + "/" + CommonPlatform.ProductName
+        ActivePlatform = Project + "/OpenBoardPkg.dsc"
+
+        self.env.SetValue("BLD_*_PLATFORM_BOARD_PACKAGE", CommonPlatform.BoardPackage, "Platform Hardcoded")
+        self.env.SetValue("PRODUCT_NAME", CommonPlatform.ProductName, "Platform Hardcoded")
+        self.env.SetValue("BLD_*_PROJECT", Project, "Platform Hardcoded")
+        self.env.SetValue("ACTIVE_PLATFORM", ActivePlatform, "Platform Hardcoded")
+        self.env.SetValue("FLASH_MAP_FDF", "Platform/Intel/KabylakeOpenBoardPkg/KabylakeRvp3/Include/Fdf/FlashMapInclude.fdf", "Platform Hardcoded")
+        self.env.SetValue("FSP_BINARY_PATH", "Silicon/Intel/FSPS/AmberLakeFspBinPkg/", "Platform Hardcoded")
+        self.env.SetValue("WORKSPACE_SILICON", "Silicon\Intel\Tools", "Default tool chain")
+        self.env.SetValue("BIOS_INFO_GUID", "C83BCE0E-6F16-4D3C-8D9F-4D6F5A032929", "Default tool chain")
+        self.env.SetValue("TARGET_ARCH", " ".join((CommonPlatform.ArchSupported)),"Platform Hardcoded")
+        self.env.SetValue("TOOL_CHAIN_TAG", "VS2017", "Default tool chain")
+        self.env.SetValue("BOARD_PKG_PCD_DSC", "KabylakeOpenBoardPkg/KabylakeRvp3/OpenBoardPkgPcd.dsc", "Default tool chain")
         return 0
 
     def AddCommandLineOptions(self, parserObj):
@@ -140,7 +135,7 @@ class PlatformBuilder( UefiBuilder, BuildSettingsManager):
     def GetName(self):
         ''' Get the name of the repo, platform, or product being build '''
         ''' Used for naming the log file, among others '''
-        return CommonPlatform.ProductName
+        return CommonPlatform.ProductName  # CommonPlat used because environment not ready when this is called
 
     def GetLoggingLevel(self, loggerType):
         ''' Get the logging level for a given type
@@ -152,8 +147,19 @@ class PlatformBuilder( UefiBuilder, BuildSettingsManager):
         return logging.DEBUG
 
     def PlatformPreBuild(self):
-        params = " ".join((CommonPlatform.FlashMap, CommonPlatform.FspBinPath, "Fsp.fd", "0x0"))
-        ret = RunPythonScript( CommonPlatform.FspRebaseScript, params)
+
+        logging.info("Rebasing FSP")
+        # not necessary to delete the old copies like build_bios.py does, the Rebase script overwrites them
+        flashMap = os.path.realpath(self.env.GetValue("FLASH_MAP_FDF"))
+        fspPath = os.path.realpath(self.env.GetValue("FSP_BINARY_PATH"))
+        params = flashMap + " " + fspPath + " Fsp.fd 0x0"
+        ret = RunPythonScript("RebaseFspBinBaseAddress.py", params)
+        if ret != 0:
+            errorString = "RebaseFspBinBaseAddress.py returned failure!"
+            logging.critical(errorString)
+            raise Exception(errorString)
+        # not necessary to concatenate the Rebased FSPs like build_bios.py does, the Rebase script does that
+
         return ret
 
     def PlatformPostBuild(self):
